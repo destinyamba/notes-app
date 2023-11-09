c38d6841fa7561b36aaa9649c66bfc7ce9f251f5
destiny.amba <destiny.ambakederemo@workrise.com>
2023-11-09 17:18:57 +0000

    home screen animation update

--- src/scenes/home/Home.tsx
+++ src/scenes/home/Home.tsx
@@ -2,6 +2,7 @@ import { SELECTED_PAGE, notes } from "@/shared/types";
 import { ListBullets, SquaresFour } from "@phosphor-icons/react";
 import SearchBar from "@/shared/SearchBar";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SELECTED_PAGE) => void;
@@ -10,7 +11,19 @@ type Props = {
 const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1024px)");
   return (
-    <section id="home" className="gap-16 py-10 md:pb-0">
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      onViewportEnter={() => setSelectedPage(SELECTED_PAGE.HOME)}
+      id="home"
+      className="gap-16 py-10 md:pb-0"
+    >
       <div className="flex justify-center mt-28 p-0 h-2/3">
         {isAboveMediumScreens ? (
           <>
@@ -56,7 +69,7 @@ const Home = ({ setSelectedPage }: Props) => {
           <div></div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
