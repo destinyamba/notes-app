63305d28d0d12cbe6acc8f8459a66412139d806c
destiny.amba <destiny.ambakederemo@workrise.com>
2023-11-09 11:56:13 +0000

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,30 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      content: {
+        bluewaves: "url('./assets/BlueWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+        arrow: "url(./assest/Arrow.png)",
+        swirlyarrow: "url(./assest/SwirlyArrow.png)",
+        yellowwaves: "url(./assest/YellowWaves.png)",
+      },
+      colors: {
+        "primary-100": "#56ACE0",
+        "primary-200": "#FFC10D",
+        "primary-300": "#194F82",
+        "secondary-200": "#ffcd3d",
+        "secondary-300": "#11375b",
+      },
+    },
     screens: {
       sm: '480px',
       md: '768px',
       lg: '976px',
       xl: '1440px',
-    },
-    fontFamily: {
-      roboto: ["Roboto", "sans-serif"],
-    },
-    content: {
-      bluewaves: "url('./assets/BlueWaves.png')",
-      sparkles: "url('./assets/Sparkles.png')",
-      circles: "url('./assets/Circles.png')",
-      arrow: "url(./assest/Arrow.png)",
-      swirlyarrow: "url(./assest/SwirlyArrow.png)",
-      yellowwaves: "url(./assest/YellowWaves.png)",
-    },
-    colors: {
-      "primary-100": "#56ACE0",
-      "primary-200": "#FFC10D",
-      "primary-300": "#194F82",
-      "secondary-200": "#ffcd3d",
-      "secondary-300": "#11375b",
     },
   },
   plugins: [],
